02fdf378f437720cec6c80d832ea916b79114420
FrederikWvB <frederik.xiao@gmail.com>
2021-04-13 11:52:40 +0200

    Pizza er DONE!

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,29 @@
 public class Pizza {
+    String navn;
+    String pizzaindhold;
+    int pris;
+    int ID;
+
+    public Pizza(String navn, String pizzaindhold, int pris, int ID) {
+        this.navn = navn;
+        this.pizzaindhold = pizzaindhold;
+        this.pris = pris;
+        this.ID = ID;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getPizzaindhold() {
+        return pizzaindhold;
+    }
+
+    public int getPris() {
+        return pris;
+    }
+
+    public int getID() {
+        return ID;
+    }
 }
