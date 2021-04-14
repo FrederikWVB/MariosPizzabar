d08a68de59dbc6409af41663d086c84a15a36d0f
andjiao <AmandaBiao1>
2021-04-14 14:25:28 +0200

    Pizza!!!!

--- src/Menukort.java
+++ src/Menukort.java
@@ -3,8 +3,11 @@ public class Menukort {
     public static Pizza[] createMenu(){
         Pizza[] menu = new Pizza[20];
 
-        menu[0] = (new Pizza("John", 10, 1));
-
+        menu[0] = (new Pizza("Vesuvio", 57, 1));
+        menu[0] = (new Pizza("Amerikaner", 53, 2));
+        menu[0] = (new Pizza("Cacciatore", 57, 3));
+        menu[0] = (new Pizza("Carbonara", 63, 4));
+        menu[0] = (new Pizza("Dennis", 65, 5));
 
         return menu;
     }
