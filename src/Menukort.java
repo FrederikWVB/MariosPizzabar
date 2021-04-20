d6a1024afa6018cfeb62130666fad720f87805bf
andjiao <AmandaBiao1>
2021-04-20 11:18:56 +0200

    menu [0] = ingen pizza

--- src/Menukort.java
+++ src/Menukort.java
@@ -5,11 +5,12 @@ public class Menukort {
     public static Pizza[] createMenu() {
         Pizza[] menu = new Pizza[20];
 
-        menu[0] = (new Pizza("Vesuvio", 57, 1));
-        menu[1] = (new Pizza("Amerikaner", 53, 2));
-        menu[2] = (new Pizza("Cacciatore", 57, 3));
-        menu[3] = (new Pizza("Carbonara", 63, 4));
-        menu[4] = (new Pizza("Dennis", 65, 5));
+        menu[0] = (new Pizza("x",1,999999999));
+        menu[1] = (new Pizza("Vesuvio", 57, 1));
+        menu[2] = (new Pizza("Amerikaner", 53, 2));
+        menu[3] = (new Pizza("Cacciatore", 57, 3));
+        menu[4] = (new Pizza("Carbonara", 63, 4));
+        menu[5] = (new Pizza("Dennis", 65, 5));
         return menu;
     }
 
